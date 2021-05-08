45c02658870779059a069ad90137ba3ddf0baf72
Kóbor Júlia <Kbor Jlia>
2021-05-08 15:25:14 +0200

    validator works

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,31 +1,44 @@
 import {Component, OnInit} from '@angular/core';
-import {FormArray, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {FormArray, FormBuilder, FormControl, FormGroup, AbstractControl, ValidationErrors} from '@angular/forms';
+
+function longerThan6 (control: AbstractControl): ValidationErrors | null {
+if (control.value.length >= 6) {
+  return null;
+}
+const valid = control.value.length>6;
+return valid ? null : {
+  notLongEnough: true
+};
+}
 
 @Component({
-  selector: 'todo-form',
-  templateUrl: `./form.component.html`,
-  styleUrls: [
-  ]
+    selector: 'todo-form',
+    templateUrl: `./form.component.html`,
+    styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
-  myForm: FormGroup = this.fb.group({
-    todo: null,
-    todos: this.fb.array(['Update the latest version', 'Practice', 'Workout'], [Validators.required]),
-  });
-  constructor(private fb: FormBuilder) {
-  }
-  get todos(): FormArray {
-    return this.myForm.get('todos') as FormArray;
-  }
-  ngOnInit(): void {
-  }
-  onSubmit(): void {
-    console.log(this.myForm, this.myForm.value);
-    const newTodo = new FormControl(this.myForm.value.todo);
-    this.todos.push(newTodo);
-    }
-  removeTodo(index: number): void {
-    this.todos.removeAt(index);
-  }
+    myForm: FormGroup = this.fb.group({
+        todo: null,
+        todos: this.fb.array(['Update the latest version', 'Practice', 'Workout']),
+    });
 
+    constructor(private fb: FormBuilder) {
+    }
+
+    get todos(): FormArray {
+        return this.myForm.get('todos') as FormArray;
+    }
+
+    ngOnInit(): void {
+    }
+
+    onSubmit(): void {
+        console.log(this.myForm, this.myForm.value);
+        const newTodo = new FormControl(this.myForm.value.todo, [longerThan6]);
+        this.todos.push(newTodo);
+    }
+
+    removeTodo(index: number): void {
+        this.todos.removeAt(index);
+    };
 }
